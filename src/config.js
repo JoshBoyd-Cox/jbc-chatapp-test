5450d53047af3413a9d85e36191a8140c6d963b6
Josh Boyd-Cox <joshua.boyd-cox@nationwide.co.uk>
2019-05-21 16:56:41 +0100

    updated guid

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
   appId: "3469712f08a7c4", //Enter your App ID
   apiKey: "ff5594b7511f11d871ab92fb95af8935727a6062", //Enter your API KEY
-  GUID: "testgroup" // Enter your group UID
+  GUID: "theboys" // Enter your group UID
 };
